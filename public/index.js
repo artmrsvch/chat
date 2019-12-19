6b4e5cf2bfbc1f993a409d4a27c3387d233a1080
Artem_Zabirov <qwertysairex@gmail.com>
2019-12-19 16:27:07 +0300

    Added a list of active user

--- public/index.js
+++ public/index.js
@@ -1,5 +1,65 @@
 const socket = io();
+const autorizModal = document.querySelector('.autorization');
+const autorizName = document.querySelector('.input-name');
+const autorizNick = document.querySelector('.input-nick');
 
-socket.on('connect', () => {
-    console.log('Client connect asdasda')
+const chatMessage = document.querySelector('.chat-block__textarea');
+const sendChatMessage = document.querySelector('.chat-block__submit-button');
+const userList = document.querySelector('.info-block__user-list');
+
+let loginInfo = new Object;
+
+autorizModal.addEventListener('click', function (e) {
+    if (e.target.className == 'autorization-close__btn') {
+        //this.style.display = 'none'
+    } else if (e.target.className == 'autorization__submit-button') {
+        if (autorizName.value && autorizNick.value != '') {
+            loginInfo = {
+                fio: autorizName.value,
+                nick: autorizNick.value
+            };
+            socket.emit('userLogin', loginInfo);
+            this.style.display = 'none'
+        } else {
+            alert('Поля ФИО и Ник должны быть заполнены!')
+        }  
+    }
+})
+
+function appendUser (user) {
+    const userItem = document.createElement('li');
+
+    userItem.classList.add('info-block__user-item');
+    userItem.innerHTML = `<div class="info-block__user-avatar"></div>
+    <div class="info-block__user-data">
+        <span class="info-block__user-name">${user.fio}</span>
+        <span class="info-block__user-lastMassege">Тут будет ласт сообщение</span>
+    </div>`;
+    userList.appendChild(userItem);
+}
+sendChatMessage.addEventListener('click', ()=>{
+    if (chatMessage.value != '') {
+        socket.emit('userMessage', { 
+            fio: loginInfo.fio,
+            nick: loginInfo.nick,
+            msg: chatMessage.value
+         });
+         chatMessage.value = '';
+    } else {
+        alert('Пусто же, нужно что-то написать')
+    }
+})
+
+socket.on('connection', (socket) => {
+    console.log(socket);
+    
+})
+socket.on('messageAppend', (data) => {
+    console.log(data)
+})
+socket.on('usersList', users => {
+    userList.innerHTML = '';
+    users.forEach(user => {
+    appendUser(user)
+    });
 })
--- server/server.js
+++ server/server.js
@@ -7,15 +7,28 @@ const app = express();
 const server = http.createServer(app);  
 const io = socketIO(server);
 
-const publicPath = path.join(__dirname, '../public')
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 4500;
 
-
 app.use(express.static(publicPath));
-io.on('connection', () => {
-    console.log('ZALYPA');
-})
 
+let users = new Array;
+
+io.sockets.on('connection', socket => {
+    socket.on('userLogin', user => {
+        users.push(user);
+        io.sockets.emit('usersList', users)
+    });
+    socket.on('userMessage', data => {
+        console.log(data.fio, data.nick, data.msg)
+        users.forEach(user => {  //Присваиваем последнее сообщение в массив юзеров
+            if (user.nick == data.nick) {
+                user.msg = data.msg
+            }
+        })
+        socket.emit('messageAppend',data)
+    });
+})
 server.listen(port, ()=> {
     console.log(`HELLOW FIRST SERVAK NA PORTY ${port}`);
 })
